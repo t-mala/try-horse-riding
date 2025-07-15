f94ba47b536c619a7ac1ec7c9401b55a99e04f16
t-mala <216566894+t-mala@users.noreply.github.com>
2025-07-15 18:08:35 +0300

    add about page & whatsapp floating button

--- try-horse-riding/src/app/app.routes.ts
+++ try-horse-riding/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NewsComponent } from './news/news.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent },
+    {path: '', component: HomeComponent },
+    {path: 'news', component: NewsComponent },
+    {path: 'services', component: ServicesComponent },
+    {path: 'about', component: AboutComponent }
+];
